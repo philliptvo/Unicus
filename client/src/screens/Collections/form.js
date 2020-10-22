32014e232bc7b4d90e581dc439f7a4bace637e3e
Phillip Vo <phillip.t.vo@gmail.com>
2020-10-22 16:14:04 -0700

    Replace YellowBox with LogBox (Expo update)

--- client/src/screens/Collections/form.js
+++ client/src/screens/Collections/form.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { YellowBox } from 'react-native';
+import { LogBox } from 'react-native';
 
 import { CollectionForm } from '../../components/Forms';
 
 // NOTE: Warning occurs be callback (onSubmit) is passed through route params. This
 //  affects state persistence and deeplinking to screen that accept functions in params
-YellowBox.ignoreWarnings(['Non-serializable values were found in the navigation state']);
+LogBox.ignoreLogs(['Non-serializable values were found in the navigation state']);
 
 const FormScreen = ({ route, navigation }) => {
   const { title, onSubmit } = route.params;
